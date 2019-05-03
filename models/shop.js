bc1ce91e15211c6a03b869ec64c8ea47dc6278b8
Jon Phillips <jonphillips@Jons-MacBook-Pro.local>
2019-05-02 20:21:38 -0500

    created shop schema

---
+++ models/shop.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const shopSchema = new Schema({
+  name: { type: String, required: true },
+  address: String,
+  phone: String,
+  hours: String,
+  website: String,
+  img: String,
+  artists: [{
+    artistName: String,
+    instagramHandle: String,
+    rating: { type: Number, min: 1, max: 5},
+  }]
+  takesWalkIns: Boolean,
+  tags: { type: [String]},
+});
+
+const Shop = mongoose.model('Shop', shopSchema);
+
+module.exports = Shop;
