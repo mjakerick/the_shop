191a758256af3bfc3c5dfc575c1261e3e144988d
Jon Phillips <jonphillips@Jons-MacBook-Pro.local>
2019-05-06 15:01:04 -0500

    logout button functional

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -2,12 +2,6 @@ const express = require('express');
 const sessions = express.Router();
 const User = require('../models/user.js');
 
-// sessions.get('/new', (req, res) => {
-//   res.render('sessions/new.ejs') {
-//     currentUser: req.session.currentUser
-//   };
-// });
-
 sessions.get('/new', (req, res) => {
   res.render('sessions/new.ejs', {
     currentUser: req.session.currentUser
@@ -22,6 +16,12 @@ sessions.post('/', (req, res) => {
     } else {
       res.send('<a href="/">wrong password</a>')
     }
+  });
+});
+
+sessions.delete('/', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/')
   });
 });
 
