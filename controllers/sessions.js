4a0906bda19dcda44f968bc53308225f981a6957
Jon Phillips <jonphillips@Jons-MacBook-Pro.local>
2019-05-06 14:57:03 -0500

    fixed login route and link in nav.ejs

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -2,8 +2,16 @@ const express = require('express');
 const sessions = express.Router();
 const User = require('../models/user.js');
 
+// sessions.get('/new', (req, res) => {
+//   res.render('sessions/new.ejs') {
+//     currentUser: req.session.currentUser
+//   };
+// });
+
 sessions.get('/new', (req, res) => {
-  res.render('sessions/new.ejs');
+  res.render('sessions/new.ejs', {
+    currentUser: req.session.currentUser
+  });
 });
 
 sessions.post('/', (req, res) => {
